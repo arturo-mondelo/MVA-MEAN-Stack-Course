580254a98877964385ed47034172561ba1a5be38
arturo-mondelo <arturo.mondelo@hotmail.es>
2016-05-17 22:32:12 +0200

    Fixing errors...

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/App'));     //serve static assets
+app.use(express.static(__dirname + '/publico'));     //serve static assets
 app.get('*', function(req, res) {
-    res.sendfile('./App/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendfile('./publico/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 app.listen(port, function() {
